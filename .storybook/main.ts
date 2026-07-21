1ed48019036e9f68067392ad26befa9bd5b35b14
Stanislav Spuzyak <stanislav.spuzyak@gmail.com>
2026-07-20 21:58:21 -0400

    fix: correct storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../docs/**/*.mdx",
-    "../docs/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
     "@chromatic-com/storybook",
